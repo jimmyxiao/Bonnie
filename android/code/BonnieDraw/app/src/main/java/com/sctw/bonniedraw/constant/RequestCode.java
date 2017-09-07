ae9f3ff266bab84f73aeb4077415098bcf0e574d
Rin Hsu <sksly789@gmail.com>
2017-09-07 18:09:03 +0800

    刪除不要的檔案

--- android/code/BonnieDraw/app/src/main/java/com/sctw/bonniedraw/constant/RequestCode.java
+++
@@ -1,8 +0,0 @@
-package com.sctw.bonniedraw.constant;
-
-/**
- * Created by professor on 6/7/16.
- */
-public class RequestCode {
-    public static final int WRITE_EXTERNAL_STORAGE_PERMISSION = 0;
-}
