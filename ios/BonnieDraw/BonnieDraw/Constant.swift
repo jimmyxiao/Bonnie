ee9b7938bddaf9280a7c803ae4ef7ff2193b71a0
Professor <jsn_professor@hotmail.com>
2017-09-30 17:45:27 +0800

    remove debug code

--- ios/BonnieDraw/BonnieDraw/Constant.swift
+++ ios/BonnieDraw/BonnieDraw/Constant.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 let DEBUG = Bundle.main.infoDictionary?["Configuration"] as? String == "Debug"
-let PATH_BUFFER_COUNT: UInt16 = 2
+let PATH_BUFFER_COUNT: UInt16 = 20
 let POINT_BUFFER_COUNT: UInt16 = 512
 let MAX_TIMESTAMP: TimeInterval = 3
 let LENGTH_SIZE: UInt16 = 20
