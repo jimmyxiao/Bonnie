a3e1d0b3ef8f7adfd3d381c6e560b45a40157b21
ryu <ryu@sourcecode.tw>
2017-10-18 11:35:01 +0800

    1.modify worklist api xmls , add new type is collection. 2.remove not use file.

--- web/BonnieDrawClient/src/modules/works/service/worksService.js
+++
@@ -1,7 +0,0 @@
-app.factory('worksService', function(baseHttp) {
-    return {
-    	queryWorksList: function(params,callback){
-			return baseHttp.service('BDService/worksList' ,params,callback);
-		}
-    }
-})
