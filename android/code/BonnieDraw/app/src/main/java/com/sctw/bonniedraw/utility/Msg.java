4b27882c7dddf61d6215e7802aa35f1185b34228
Rin Hsu <sksly789@gmail.com>
2017-10-27 18:27:29 +0800

    補放了一個新增的JAVA檔案

---
+++ android/code/BonnieDraw/app/src/main/java/com/sctw/bonniedraw/utility/Msg.java
@@ -0,0 +1,16 @@
+package com.sctw.bonniedraw.utility;
+
+import java.sql.Timestamp;
+
+/**
+ * Created by Fatorin on 2017/10/27.
+ */
+
+public class Msg {
+    private int worksMsgId;
+    private int worksId;
+    private int userId;
+    private String message;
+    private int msgOrder;
+    private Timestamp creationDate;
+}
