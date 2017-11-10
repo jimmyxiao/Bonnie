20e147c147f846ff15a9f58534df5f432139024f
ryu <ryu@sourcecode.tw>
2017-11-10 13:58:05 +0800

    revise singup controller.

--- web/BonnieDrawClient/src/modules/login/controller/registerController.js
+++ web/BonnieDrawClient/src/modules/login/controller/registerController.js
@@ -1,11 +1,4 @@
-app.factory('registerService', function(baseHttp) {
-    return {
-        register : function(params, callback) {
-            return baseHttp.service('BDService/login', params, callback);
-        }
-    }
-})
-.controller('registerController', function ($scope, $rootScope, $location, $cookieStore, $window, $state, $http, registerService, AuthenticationService) {
+app.controller('registerController', function ($scope, $rootScope, $location, $cookieStore, $window, $state, $http, loginService, AuthenticationService) {
     	$rootScope.title = '註冊 | BonnieDRAW';
         $scope.register = {
     		phoneNo:null, userName:null, userCode:null, userPw:null, userType:1 
@@ -21,10 +14,10 @@ app.factory('registerService', function(baseHttp) {
                     dt:3    
                 }
                 params.fn = 3;
-	            registerService.register(params, function(data, status, headers, config) {
+	            loginService.login(params, function(data, status, headers, config) {
 	            	if(data.res == 1){
                         params.fn = 2;
-	            		registerService.register(params, function(data, status, headers, config) {
+	            		loginService.login(params, function(data, status, headers, config) {
                             if(data.res == 1){
                                 alert('註冊成功,信件已發送!');
                                 $state.go('login');
