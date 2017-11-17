d7eeec5428f55d0b21667827ce310c5b01cc11d5
andrew <s83060207@yahoo.com.tw>
2017-11-17 13:09:48 +0800

    myfile 新增 mywork,fans,follow,Collection 切換資料(未串api)

--- web/BonnieDrawClient/src/modules/user/controller/myfileController.js
+++ web/BonnieDrawClient/src/modules/user/controller/myfileController.js
@@ -2,7 +2,19 @@ app.controller('myfileController', function ($cookieStore, $rootScope, $scope, $
 		$rootScope.title = '我的帳戶 | BonnieDRAW';
 		$rootScope.nav = '';
 		$('#loader-container').fadeOut("slow");
+		$scope.workclick =true;//works,Collection follow,fans div切換
+		$scope.usernav='';
 		new WOW().init();
+		$rootScope.maxPagination = 0;
+		$rootScope.paginList =[];
+		$scope.clickPagin = function(pagin){
+			$rootScope.offset = pagin;
+			$scope.queryPopularWorks();
+		}
+		$scope.movePagin = function(offset){
+			$rootScope.offset = $rootScope.offset + offset;
+			$scope.queryPopularWorks();
+		}
 		
 		$scope.queryUser = function(){
 			var params = util.getInitalScope();
@@ -11,24 +23,128 @@ app.controller('myfileController', function ($cookieStore, $rootScope, $scope, $
 				if(data.res == 1){
 					$scope.user = data;
 					$scope.queryMyWorks();
+					console.log(data);
 				}
 			})
 		}
 		$scope.queryUser();
 
-		$scope.queryMyWorks = function(){
-			$scope.worksList = [];
-			var params = util.getInitalScope();
-			params.wid = 0;
-			params.wt = 5;
-			params.stn = $scope.offset;
-			params.rc = 6; 
-			worksService.queryWorksList(params,function(data, status, headers, config){
-				if(data.res == 1){
-					$scope.worksList = data.workList;
+		$scope.queryMyWorks = function(tag){
+			if($scope.usernav!='w'){
+				$scope.usernav = tag;
+				$cookieStore.put('offset',0);
+		 		if($cookieStore.get('offset') > 1){
+					$rootScope.offset = $cookieStore.get('offset');
+				}else{
+					$rootScope.offset = 1;
 				}
-			})
+				$rootScope.maxPagination = 0;
+				$rootScope.paginList =[];
+
+				$scope.worksList = [];
+				var params = util.getInitalScope();
+				params.wid = 0;
+				params.wt = 5;
+				params.stn = $scope.offset;
+				params.rc = 16; 
+				worksService.queryWorksList(params,function(data, status, headers, config){
+					if(data.res == 1){
+						$rootScope.maxPagination = data.maxPagination;
+						$rootScope.initalPaginList();
+						$scope.worksList = data.workList;
+						$scope.dataList = $scope.worksList;
+						$scope.workclick =true;
+					}
+				})
+			}
 		}
+
+		$scope.queryCollectionWorks = function(tag){
+			if($scope.usernav!='fa'){
+				$scope.usernav = tag;
+				$cookieStore.put('offset',0);
+		 		if($cookieStore.get('offset') > 1){
+					$rootScope.offset = $cookieStore.get('offset');
+				}else{
+					$rootScope.offset = 1;
+				}
+				$rootScope.maxPagination = 0;
+				$rootScope.paginList =[];
+
+				$scope.mainSectionArr = [];
+				var params = util.getInitalScope();
+				params.wid = 0;
+				params.wt = 7;
+				params.stn = $scope.offset;
+				params.rc = 16; 
+				worksService.queryWorksList(params,function(data, status, headers, config){
+					if(data.res == 1){
+						$rootScope.maxPagination = data.maxPagination;
+						$rootScope.initalPaginList();
+						$scope.mainSectionArr = data.workList;
+						$scope.dataList = $scope.mainSectionArr;
+						$scope.workclick =true;
+					}
+				})
+			}
+		}
+		$scope.followWorks = function(tag) {
+			if($scope.usernav!='fo'){
+				$scope.usernav = tag;
+				$cookieStore.put('offset',0);
+		 		if($cookieStore.get('offset') > 1){
+					$rootScope.offset = $cookieStore.get('offset');
+				}else{
+					$rootScope.offset = 1;
+				}
+				$rootScope.maxPagination = 0;
+				$rootScope.paginList =[];
+
+				$scope.followList = [];
+				var params = util.getInitalScope();
+				params.stn = $scope.offset;
+				params.rc = 12; 
+				params.fn = 1;
+				worksService.queryfollowingList(params,function(data, status, headers, config){
+					if(data.res == 1){
+						$rootScope.maxPagination = data.maxPagination;
+						$rootScope.initalPaginList();
+						$scope.followList = data.workList;
+						$scope.dataList = $scope.followList;
+						$scope.workclick =false;
+					}
+				})
+			}
+		}
+		$scope.fansWorks = function(tag) {
+			if($scope.usernav!='c'){
+				$scope.usernav = tag;
+				$cookieStore.put('offset',0);
+		 		if($cookieStore.get('offset') > 1){
+					$rootScope.offset = $cookieStore.get('offset');
+				}else{
+					$rootScope.offset = 1;
+				}
+				$rootScope.maxPagination = 0;
+				$rootScope.paginList =[];
+
+				$scope.fansList = [];
+				var params = util.getInitalScope();
+				params.stn = $scope.offset;
+				params.rc = 12; 
+				params.fn = 2;
+				worksService.queryfollowingList(params,function(data, status, headers, config){
+					if(data.res == 1){
+						$rootScope.maxPagination = data.maxPagination;
+						$rootScope.initalPaginList();
+						$scope.fansList = data.workList;
+						$scope.dataList = $scope.fansList;
+						$scope.workclick =false;
+					}
+				})
+			}
+		}
+		
 
 	}
 )
