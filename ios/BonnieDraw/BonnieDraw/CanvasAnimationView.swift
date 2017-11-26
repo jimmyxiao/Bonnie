5377343e1b3550e967aa8233aee572dd3b2c9423
Professor <jsn_professor@hotmail.com>
2017-11-27 00:26:53 +0800

    modify animation duration logic

--- ios/BonnieDraw/BonnieDraw/CanvasAnimationView.swift
+++ ios/BonnieDraw/BonnieDraw/CanvasAnimationView.swift
@@ -172,6 +172,7 @@ class CanvasAnimationView: UIView {
                     self.drawToPersistentImage(withBounds: bounds)
                 }
             }
+            timer?.invalidate()
             timer = Timer.scheduledTimer(withTimeInterval: point.duration, repeats: false) {
                 timer in
                 self.setNeedsDisplay()
