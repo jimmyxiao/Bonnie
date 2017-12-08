f9298c7fa63281976b21702af0dd5f9699cea1c9
ryu <ryu@sourcecode.tw>
2017-12-08 18:13:03 +0800

    service修正小問題

--- web/BonnieDrawService/src/com/bonniedraw/notification/controller/PushNotificationsController.java
+++ web/BonnieDrawService/src/com/bonniedraw/notification/controller/PushNotificationsController.java
@@ -1,22 +1,22 @@
 package com.bonniedraw.notification.controller;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+//import java.util.concurrent.CompletableFuture;
+//import java.util.concurrent.ExecutionException;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpEntity;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RestController;
 
-import com.bonniedraw.notification.service.PushNotificationsService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+//import com.bonniedraw.notification.service.PushNotificationsService;
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.fasterxml.jackson.databind.JsonNode;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @Controller
 public class PushNotificationsController {
--- web/BonnieDrawService/src/com/bonniedraw/works/module/WorksMsgModule.java
+++ web/BonnieDrawService/src/com/bonniedraw/works/module/WorksMsgModule.java
@@ -4,6 +4,7 @@ import com.bonniedraw.works.model.WorksMsg;
 
 public class WorksMsgModule extends WorksMsg{
 	private String userName;
+	private String profilePicture;
 
 	public String getUserName() {
 		return userName;
@@ -11,6 +12,14 @@ public class WorksMsgModule extends WorksMsg{
 
 	public void setUserName(String userName) {
 		this.userName = userName;
+	}
+
+	public String getProfilePicture() {
+		return profilePicture;
+	}
+
+	public void setProfilePicture(String profilePicture) {
+		this.profilePicture = profilePicture;
 	}
 	
 }
