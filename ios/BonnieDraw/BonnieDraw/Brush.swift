855140266144dc6aaa7834b19cc6bb797c6cebc1
Professor <jsn_professor@hotmail.com>
2018-01-22 21:08:22 +0800

    remove pen and airbrush rotation calculation

--- ios/BonnieDraw/BonnieDraw/Brush.swift
+++ ios/BonnieDraw/BonnieDraw/Brush.swift
@@ -19,7 +19,7 @@ class Brush: JotBrushTexture {
     }
     var maxSize: CGFloat, minAlpha: CGFloat, maxAlpha: CGFloat
     var color = UIColor.black
-    var isRotationSupported = true, isForceSupported = false, isVelocitySupported = false
+    var isRotationSupported = false, isForceSupported = false, isVelocitySupported = false
     var stepWidth: CGFloat = 1
     var smoothness: CGFloat = 1
     var type: Type {
@@ -28,6 +28,7 @@ class Brush: JotBrushTexture {
             if customBrush != nil {
                 customBrush = nil
             }
+            isRotationSupported = type == .pencil || type == .crayon || type == .marker
         }
     }
 
